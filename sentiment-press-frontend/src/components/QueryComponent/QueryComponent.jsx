1c863e50e31e8eaa909e99236d6fb7117ce4fd3c
Rumble <rumble@Admins-MacBook-Pro.local>
2025-01-21 18:05:28 +0000

    Carousel for top/bottom 3 articles FE.

--- sentiment-press-frontend/src/components/QueryComponent/QueryComponent.jsx
+++ sentiment-press-frontend/src/components/QueryComponent/QueryComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Slider from "react-slick"; // Import react-slick
 import './Query.css';
 import { FcNegativeDynamic } from "react-icons/fc";
 import { FcPositiveDynamic } from "react-icons/fc";
@@ -46,126 +47,128 @@ const QueryComponent = () => {
     }
   };
 
+  // Slick carousel settings
+  const carouselSettings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  };
+
   return (
     <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
       <h1>Search Query</h1>
-                <form onSubmit={handleQuerySubmit}>
-            <div style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
-                <input
-                type="text"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                placeholder="Enter your query"
-                style={{
-                    width: '100%',
-                    padding: '10px',
-                    fontSize: '16px',
-                    boxSizing: 'border-box',
-                }}
-                />
-                <button
-                type="submit"
-                style={{
-                    background: 'none',
-                    border: 'none',
-                    cursor: 'pointer',
-                    fontSize: '16px',
-                    display: 'flex',    /* Make the button a flex container */
-                    justifyContent: 'center', /* Center the icon horizontally */
-                    alignItems: 'center', /* Align the icon vertically */
-                    marginLeft: '4px', /* Optional: space between input and button */
-                }}
-                >
-              <IoSearchCircle style={{ fontSize: '50px', color: '#920089', }} /> 
-                </button>
-            </div>
-            </form>
-
+      <form onSubmit={handleQuerySubmit}>
+        <div style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Enter your query"
+            style={{
+              width: '100%',
+              padding: '10px',
+              fontSize: '16px',
+              boxSizing: 'border-box',
+            }}
+          />
+          <button
+            type="submit"
+            style={{
+              background: 'none',
+              border: 'none',
+              cursor: 'pointer',
+              fontSize: '16px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginLeft: '4px',
+            }}
+          >
+            <IoSearchCircle style={{ fontSize: '50px', color: '#920089' }} />
+          </button>
+        </div>
+      </form>
 
       {error && <div style={{ color: 'red', marginTop: '10px' }}>{error}</div>}
 
       {result && (
         <div style={{ marginTop: '20px' }}>
           <div className="query-data">
-
-        <div className="grid">
-        <div className="data-column">
-            {/* Nested Grid with Two Columns */}
-            <div className="nested-data">
-            <div className="nested-data-item">
-                <div className="grid-item">
-                <BiAnalyse style={{color: '#008000'}} />    Articles Analysed: {result.query_info.total_articles}
+            <div className="grid">
+              <div className="data-column">
+                <div className="nested-data">
+                  <div className="nested-data-item">
+                    <div className="grid-item">
+                      <BiAnalyse style={{ color: '#008000' }} /> Articles Analysed: {result.query_info.total_articles}
+                    </div>
+                    <div className="grid-item">
+                      <FcPositiveDynamic /> Positive Count: {result.query_info.positive_count}
+                    </div>
+                    <div className="grid-item">
+                      <FcNegativeDynamic /> Negative Count: {result.query_info.negative_count}
+                    </div>
+                  </div>
+                  <div className="nested-data-item">
+                    <div className="grid-item2">
+                      <Speedometer value={parseFloat(result.query_info.mean_sentiment)} />
+                    </div>
+                  </div>
                 </div>
-                <div className="grid-item">
-                <FcPositiveDynamic />    Positive Count: {result.query_info.positive_count}
-                </div>
-                <div className="grid-item">
-                <FcNegativeDynamic />    Negative Count: {result.query_info.negative_count}
-                </div>
-                
+              </div>
             </div>
-            <div className="nested-data-item">
-            <div className="grid-item2">
-                <Speedometer value={parseFloat(result.query_info.mean_sentiment)} />
-                </div>
-            </div>
-        </div>
-        </div>
-        </div>
-
 
             <div className="summary">
-            <div className="nested-grid">
-                    <div
-                        className={`nested-item ${activeSection === 'summary' ? 'active' : ''}`}
-                        onClick={() => setActiveSection('summary')}
-                    >
-                        
-                        <SiCoveralls style={{color: '#89CFF0'}}/>  Summary
-                    </div>
-                    <div
-                        className={`nested-item ${activeSection === 'top3' ? 'active' : ''}`}
-                        onClick={() => setActiveSection('top3')}
-                    >
-                        <FcPositiveDynamic />   Top 3 Articles 
-                    </div>
-                    <div
-                        className={`nested-item ${activeSection === 'bottom3' ? 'active' : ''}`}
-                        onClick={() => setActiveSection('bottom3')}
-                    >
-                       <FcNegativeDynamic />   Bottom 3 Articles 
-                    </div>
-                    </div>
+              <div className="nested-grid">
+                <div
+                  className={`nested-item ${activeSection === 'summary' ? 'active' : ''}`}
+                  onClick={() => setActiveSection('summary')}
+                >
+                  <SiCoveralls style={{ color: '#89CFF0' }} /> Summary
+                </div>
+                <div
+                  className={`nested-item ${activeSection === 'top3' ? 'active' : ''}`}
+                  onClick={() => setActiveSection('top3')}
+                >
+                  <FcPositiveDynamic /> Top 3 Articles
+                </div>
+                <div
+                  className={`nested-item ${activeSection === 'bottom3' ? 'active' : ''}`}
+                  onClick={() => setActiveSection('bottom3')}
+                >
+                  <FcNegativeDynamic /> Bottom 3 Articles
+                </div>
+              </div>
 
               <div className="nested-content">
-                {activeSection === 'summary' && (
-                  <p>Summary: {result.query_info.summary}</p>
-                )}
+                {activeSection === 'summary' && <p>Summary: {result.query_info.summary}</p>}
                 {activeSection === 'top3' && (
-                  <ul>
+                  <Slider {...carouselSettings}>
                     {result.top3.map((article, index) => (
-                      <li key={index}>
+                      <div key={index} className="carousel-item">
                         <strong>{article.title}</strong>
                         <p>Source: {article.source}</p>
                         <p>Sentiment: {article.sentiment}</p>
                         <p>{article.description}</p>
                         <p>Published Date: {article.published_date}</p>
-                      </li>
+                      </div>
                     ))}
-                  </ul>
+                  </Slider>
                 )}
                 {activeSection === 'bottom3' && (
-                  <ul>
+                  <Slider {...carouselSettings}>
                     {result.bottom3.map((article, index) => (
-                      <li key={index}>
+                      <div key={index} className="carousel-item">
                         <strong>{article.title}</strong>
                         <p>Source: {article.source}</p>
                         <p>Sentiment: {article.sentiment}</p>
                         <p>{article.description}</p>
                         <p>Published Date: {article.published_date}</p>
-                      </li>
+                      </div>
                     ))}
-                  </ul>
+                  </Slider>
                 )}
               </div>
             </div>
